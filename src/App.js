aba0988131be37e2a4e6d23b1597567c57a8b735
shamimspro <shamimspro@gmail.com>
2022-09-20 17:34:55 +0600

    react styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 
+const styleObject = {
+  color: "red",
+  backgroundColor: "pink"
+};
+
 function App() {
   return (
     <div className="App">
+      {/* Inline Styling */}
+      <h2 style={{color: "blue", backgroundColor: "yellow"}}>Hello World!</h2>
+      {/* Internal Styling */}
+      <h2 style={styleObject}>Hello Bangladesh.</h2>
+      {/* External Styling */}
+      <h2>Hello Rangpur.</h2>
     </div>
   );
 }
